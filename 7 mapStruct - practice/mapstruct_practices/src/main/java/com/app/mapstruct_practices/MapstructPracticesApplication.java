c03ce21c85ce751fa459db5ff3bd22c9013ec233
JL-Gale <jorge.galem@gmail.com>
2025-04-22 16:56:40 -0500

    mapStruct practice

---
+++ 7 mapStruct - practice/mapstruct_practices/src/main/java/com/app/mapstruct_practices/MapstructPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.app.mapstruct_practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructPracticesApplication.class, args);
+	}
+
+}
---
+++ 7 mapStruct - practice/mapstruct_practices/src/test/java/com/app/mapstruct_practices/MapstructPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mapstruct_practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

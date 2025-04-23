14e6f4cde1eb32a03ceb1202ae900c36263dafae
JL-Gale <jorge.galem@gmail.com>
2025-04-23 13:30:09 -0500

    Refactor pom

--- 7 mapStruct - practice/mapstruct_practices/src/main/java/com/app/MapstructPracticesApplication.java
+++ 7 mapStruct - practice/mapstruct_practices/src/main/java/com/app/MapstructPracticesApplication.java
@@ -1,7 +1,12 @@
-package com.app.mapstruct_practices;
+package com.app;
 
+import com.app.practice01.repository.IUserRepository;
+import com.app.practice01.repository.UserRepositoryImpl;
+import com.app.practice01.service.IUserService;
+import com.app.practice01.service.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MapstructPracticesApplication {
@@ -10,4 +15,13 @@ public class MapstructPracticesApplication {
 		SpringApplication.run(MapstructPracticesApplication.class, args);
 	}
 
+	@Bean
+	public IUserRepository iUserRepository() {
+		return new UserRepositoryImpl();
+	}
+
+	@Bean
+	public IUserService iUserService(IUserRepository iUserRepository) {
+		return new UserServiceImpl(iUserRepository);
+	}
 }
